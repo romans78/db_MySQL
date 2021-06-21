8eee4f34f41cde1508a0d6e2b27a04492e5536c8
roman <roman.sizov@gmail.com>
2021-06-20 20:54:20 -0400

    hw5

--- hw5.sql
+++ hw5.sql
@@ -2,7 +2,7 @@
 1. Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их
 текущими датой и временем.
 */
- 
+
 update users set created_at=now(), updated_at=now();
 
 /*
